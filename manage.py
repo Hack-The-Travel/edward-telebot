275d994bd391635b99767964ce2f8d26c1375561
Sergey Popinevskiy <gurza000@gmail.com>
2018-06-11 21:10:41 +0300

    Stored additional information about chat

--- manage.py
+++ manage.py
@@ -7,8 +7,11 @@ if __name__ == '__main__':
     db_connection = sqlite3.connect(DB_NAME)
     cursor = db_connection.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS chat (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        chat_id INTEGER NOT NULL,
+        id INTEGER PRIMARY KEY,
+        username VARCHAR,
+        first_name VARCHAR,
+        last_name VARCHAR,
+        type VARCHAR,
         created_at integer(4) not null default (strftime('%s','now'))
     )''')
     db_connection.commit()
