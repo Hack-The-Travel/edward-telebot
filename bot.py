51af752fe881967cc74c94bdbbd09c3b8deb5369
Sergey Popinevskiy <gurza000@gmail.com>
2018-06-11 21:30:50 +0300

    Refactoring

--- bot.py
+++ bot.py
@@ -6,30 +6,33 @@ from conf import TOKEN, DB_NAME
 bot = telebot.TeleBot(TOKEN)
 
 
+def execute_sql(query):
+    db_connection = sqlite3.connect(DB_NAME)
+    cursor = db_connection.cursor()
+    cursor.execute(query)
+    rowcount = cursor.rowcount
+    db_connection.commit()
+    db_connection.close()
+    return rowcount
+
+
 @bot.message_handler(commands=['subscribe'])
 def command_subscribe(message):
     chat_id = message.chat.id
-    db_connection = sqlite3.connect(DB_NAME)
-    cursor = db_connection.cursor()
-    cursor.execute("INSERT OR IGNORE INTO chat (id, username, first_name, last_name, type)"
-                   "VALUES ({id}, '{username}', '{first_name}', '{last_name}', '{type}')"
-                   .format(id=chat_id, username=message.chat.username, first_name=message.chat.first_name,
-                           last_name=message.chat.last_name, type=message.chat.type))
-    if cursor.rowcount:
+    query = '''INSERT OR IGNORE INTO chat (id, username, first_name, last_name, type)
+               VALUES ({id}, '{username}', '{first_name}', '{last_name}', '{type}')
+            '''.format(id=chat_id, username=message.chat.username, first_name=message.chat.first_name,
+                       last_name=message.chat.last_name, type=message.chat.type)
+    if execute_sql(query):
         print('New connection:', chat_id)
-    db_connection.commit()
-    db_connection.close()
     bot.send_message(chat_id, 'You have been subscribed.')
 
 
 @bot.message_handler(commands=['unsubscribe'])
 def command_unsubscribe(message):
     chat_id = message.chat.id
-    db_connection = sqlite3.connect(DB_NAME)
-    cursor = db_connection.cursor()
-    cursor.execute("DELETE from chat where id={}".format(chat_id))
-    db_connection.commit()
-    db_connection.close()
+    query = 'DELETE from chat where id={}'.format(chat_id)
+    execute_sql(query)
     bot.send_message(chat_id, 'You have been unsubscribed.')
 
 
