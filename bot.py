2d392353f308f53759570770a5a8c0a14e26569f
Sergey Popinevskiy <gurza000@gmail.com>
2018-06-11 18:29:00 +0300

    Implemented handlers for `start` and `help` commands

---
+++ bot.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import telebot
+from .conf import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    print('New connection:', message.chat.id)
+    command_help(message)
+
+
+@bot.message_handler(commands=['help'])
+def command_help(message):
+    chat_id = message.chat.id
+    bot.send_message(
+        chat_id,
+        'I can help you keep up to date with news from Russian online travel market. Add me to contacts and wait...'
+    )
+
+
+bot.polling()
