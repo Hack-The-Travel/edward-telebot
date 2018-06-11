329d3bdf70e367db17d3d0ed94078747300ccd48
Sergey Popinevskiy <gurza000@gmail.com>
2018-06-11 21:11:32 +0300

    Implemented `subscribe` command

--- bot.py
+++ bot.py
@@ -6,18 +6,20 @@ from conf import TOKEN, DB_NAME
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['subscribe'])
 def command_start(message):
     chat_id = message.chat.id
     db_connection = sqlite3.connect(DB_NAME)
     cursor = db_connection.cursor()
-    cursor.execute('SELECT chat_id FROM chat where chat_id={}'.format(chat_id))
-    if len(cursor.fetchall()) == 0:
-        cursor.execute('INSERT INTO chat (chat_id) VALUES ({})'.format(chat_id))
-        db_connection.commit()
+    cursor.execute("INSERT OR IGNORE INTO chat (id, username, first_name, last_name, type)"
+                   "VALUES ({id}, '{username}', '{first_name}', '{last_name}', '{type}')"
+                   .format(id=chat_id, username=message.chat.username, first_name=message.chat.first_name,
+                           last_name=message.chat.last_name, type=message.chat.type))
+    if cursor.rowcount:
         print('New connection:', chat_id)
+    db_connection.commit()
     db_connection.close()
-    command_help(message)
+    bot.send_message(chat_id, 'You have been subscribed.')
 
 
 @bot.message_handler(commands=['help'])
