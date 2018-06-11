7eff2dd537be26e5351a39bf84811def0036c894
Sergey Popinevskiy <gurza000@gmail.com>
2018-06-11 19:40:56 +0300

    Implemented script to manage db store

---
+++ manage.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+from conf import DB_NAME
+
+
+if __name__ == '__main__':
+    db_connection = sqlite3.connect(DB_NAME)
+    cursor = db_connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS chat (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        chat_id INTEGER NOT NULL
+    )''')
+    db_connection.commit()
