a61f1c565976f0f6e3c542d37bba08a5aba3ceeb
Sergey Popinevskiy <gurza000@gmail.com>
2018-06-15 16:42:54 +0300

    Added sample of config

---
+++ conf.sample.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# It's sample config
+# Add conf.py with correct values
+TOKEN = '999999999:ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'  # ask @BotFather
+DB_NAME = 'edward.db'
+ADMIN_IDS = []
