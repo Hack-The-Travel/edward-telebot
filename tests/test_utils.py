ea600cc51e45e1250f27bb8c81d73ba144db264f
Sergey Popinevskiy <gurza000@gmail.com>
2018-08-26 15:32:58 +0300

    Add case 'day - days'

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,6 +7,7 @@ class TestUtils():
     @pytest.mark.parametrize(
         'start_time, check_time, runtime_str', (
             (1535285964, 1535199564, 'Up 1 day.'),
+            (1535285964, 1535113164, 'Up 2 days.'),
         ))
     def test_runtime(self, start_time, check_time, runtime_str):
         assert runtime(start_time=start_time, check_time=check_time) == runtime_str
