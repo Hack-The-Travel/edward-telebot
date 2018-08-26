9cd157d7cf4a222e02e26d457d04945b2f848923
Sergey Popinevskiy <gurza000@gmail.com>
2018-08-26 15:55:50 +0300

    Now, indeed, are all cases

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,6 +6,13 @@ from bot import runtime
 class TestUtils():
     @pytest.mark.parametrize(
         'start_time, check_time, runtime_str', (
+            (1535285964, 1535285964, 'Up 0 second.'),
+            (1535285964, 1535286024, 'Up 1 second.'),
+            (1535285964, 1535286084, 'Up 2 seconds.'),
+            (1535285964, 1535289564, 'Up 1 minute.'),
+            (1535285964, 1535289564, 'Up 2 minutes.'),
+            (1535285964, 1535289564, 'Up 1 hour.'),
+            (1535285964, 1535293164, 'Up 2 hours.'),
             (1535285964, 1535372364, 'Up 1 day.'),
             (1535285964, 1535375964, 'Up 1 day, 1 hour.'),
             (1535285964, 1535379564, 'Up 1 day, 2 hours.'),
