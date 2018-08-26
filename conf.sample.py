c4236ae159b0ac9b38265d35e626d25052c401b1
Sergey Popinevskiy <gurza000@gmail.com>
2018-08-26 09:56:37 +0300

    Error handler for broadcasting (#10)
    Implement error handler for broadcasting to prevent infinity loop with message sending.

--- conf.sample.py
+++ conf.sample.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # It's sample config
 # Add conf.py with correct values
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+
 TOKEN = '999999999:ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'  # ask @BotFather
-DB_NAME = 'edward.db'
+DB_NAME = os.path.join(here, 'db/edward.db')
 ADMIN_IDS = []
